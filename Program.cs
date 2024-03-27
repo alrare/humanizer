c7d9dc96f7f9a94b3782b217eb7d3ef3473cabb2
Alejandro Ramirez Reyes Broxel <alejandroramirezreyesbroxel@EXT-G64NW12315.local>
2024-03-27 15:19:26 -0600

    firts commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Humanizer;
+using System.Globalization;
+
+var culture = new CultureInfo("es-ES");
+int amount = 2750; 
+
+Console.WriteLine($"Cantidad en letra: {amount.ToWords(culture)} pesos 35/100 M.N.");
